ce3164dcfd71148523b3906f683f16db30740477
anniec9205 <danbi9205@gmail.com>
2021-05-08 23:11:51 +1000

    added intern js

---
+++ Library/intern.js
@@ -0,0 +1,17 @@
+const Employee = require("./Employee.js");
+
+class Intern extends Employee {
+    constructor(name, email, id, school) {
+        super(name, id, email);
+        this.school = school;
+    }
+    getRole() {
+        return "intern";
+    }
+    
+    getSchool() {
+        return this.school;
+    }
+};
+
+module.exports = Intern;
